fe449e36f0a7e5b9ee52a7459910319351163b79
Adithya Menon <168117152+AdithyaMenon-ai@users.noreply.github.com>
2026-09-13 15:51:07 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 19.3 MB (55.07%)

---
+++ leetcode/0782-jewels-and-stones/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        jewels_set = set(jewels)
+        count=0
+        for i in stones:
+            if i in jewels_set:
+                count+=1
+        return count
+        
