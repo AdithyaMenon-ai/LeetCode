7c0380c87cc5909bcaea8458a65da0f961382b5d
Adithya Menon <168117152+AdithyaMenon-ai@users.noreply.github.com>
2026-09-21 14:46:27 +0000

    [LeetCode] Runtime - 0 ms (100.00%), Memory - 19.3 MB (82.67%)

--- leetcode/1791-richest-customer-wealth/solution.py
+++ leetcode/1791-richest-customer-wealth/solution.py
@@ -2,6 +2,7 @@ class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
         maximum=0
         for i in accounts:
-            if sum(i)>maximum:
+            wealth=sum(i)
+            if wealth>maximum:
                 maximum=sum(i)
         return maximum        
